44404f3a37ada5326fe63bc1ce16216d59f2a333
wodiesan <wodiesan@gmail.com>
2016-03-03 23:55:39 -0500

    Add ATtiny85 night vision code.

--- night_sensor/night_feature_attiny85.ino
+++ night_sensor/night_feature_attiny85.ino
@@ -4,13 +4,10 @@
 @Desc:       Adafruit Analog Light Sensor, modified by Ron Chau.
 @Misc:       This version uses a stand-alone ATtiny85.
 
- 1. Connect sensor output to Analog Pin 0
+ 1. Connect sensor output to Analog Pin 1
  2. Connect 5v to VCC and GND to GND
  3. Connect 3.3v to the AREF pin
 */
-
-//int led1 = 2;          // LED connected to digital pin 2 (Uno)
-//int sensorPin = A0;    // Sensor connected to pin A0 (Uno)
 
 int led1 = 1;          // LED connected to digital pin 1 (physical pin 6)
 int led2 = 3;          // LED connected to digital pin 3 (physical pin 2)
@@ -39,14 +36,8 @@ void loop()
 {
   // Read the raw value from the sensor:
   int rawValue = analogRead(sensorPin);
-
-  //Serial.print("Raw = ");
-  //Serial.print(rawValue);
-  //Serial.print(" - Lux = ");
-  //Serial.println(RawToLux(rawValue));
   int luxValue = (RawToLux(rawValue));
-  //Serial.print("luxVal = ");
-  //Serial.println(luxValue);
+
   delay(1000);
 
   // LEDS on if lux less than or equal.
@@ -60,7 +51,6 @@ void loop()
     digitalWrite(led2, LOW);
     digitalWrite(led3, LOW);
   }
-  //delay(1000);
 }
 
 float RawToLux(int raw)
